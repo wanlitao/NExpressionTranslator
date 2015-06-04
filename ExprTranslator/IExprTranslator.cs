95b1d8621295956b5c8f08b7edb36db701b15b46
wanlitao <wanlitao_3055601@outlook.com>
2015-06-04 10:01:30 +0800

    renmae project

---
+++ ExprTranslator/IExprTranslator.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ExprTranslator
+{
+    /// <summary>
+    /// 表达式翻译接口
+    /// </summary>
+    public interface IExprTranslator
+    {
+        /// <summary>
+        /// 翻译成字符串
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        string Translate(Expression expression);
+    }
+}
