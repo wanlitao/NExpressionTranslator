767bb792149d39275e0b0770d233ae3b4539409f
wanlitao <wanlitao_3055601@outlook.com>
2015-06-04 11:05:14 +0800

    add query translator

---
+++ ExprTranslator.Data/QueryTranslator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExprTranslator.Data
+{
+    /// <summary>
+    /// 查询翻译器
+    /// </summary>
+    public class QueryTranslator : ExpressionVisitor, IExprTranslator
+    {
+        public string Translate(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
