8bd5605bb1c88a4ed1a9b598bdaf20c7b09a9af6
wanlitao <wanlitao_3055601@outlook.com>
2015-06-30 20:07:20 +0800

    add nuget deploy

--- ExprTranslator.Query/Properties/AssemblyInfo.cs
+++ ExprTranslator.Query/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ExprTranslator.Data")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ExprTranslator.Query")]
+[assembly: AssemblyDescription("dotnet expression translator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ExprTranslator.Data")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("GreatBillows")]
+[assembly: AssemblyProduct("NExprTranslator")]
+[assembly: AssemblyCopyright("Copyright © GreatBillows 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
--- ExprTranslator/Properties/AssemblyInfo.cs
+++ ExprTranslator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ExprTranslator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("dotnet expression translator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ExprTranslator")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCompany("GreatBillows")]
+[assembly: AssemblyProduct("NExprTranslator")]
+[assembly: AssemblyCopyright("Copyright © GreatBillows 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
